fe3404bd2600b1d21269f25ff003b8f7af40bacc
Nuhan Ahnaf <nuhan.ahanaf@gmail.com>
2020-09-09 21:56:29 -0500

    Updates

---
+++ precache-manifest.6e2d47c78cd10f02020423ce0084ced3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71483e8b494c3bad885eda5a10f5b6e",
+    "url": "/mypage/index.html"
+  },
+  {
+    "revision": "02d592d66f725af594fb",
+    "url": "/mypage/static/css/main.faa3933e.chunk.css"
+  },
+  {
+    "revision": "6777f62b500dcc8f276b",
+    "url": "/mypage/static/js/2.93a1a413.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mypage/static/js/2.93a1a413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d592d66f725af594fb",
+    "url": "/mypage/static/js/main.fcabd8ae.chunk.js"
+  },
+  {
+    "revision": "f62fe814d257ca801b20",
+    "url": "/mypage/static/js/runtime-main.cc316ea1.js"
+  },
+  {
+    "revision": "551949d738ababc9fb73dae9ff199d0a",
+    "url": "/mypage/static/media/coves_bold-webfont.551949d7.woff"
+  },
+  {
+    "revision": "bb31e072c19250bf4f8ad971c8d8dfee",
+    "url": "/mypage/static/media/coves_light-webfont.bb31e072.woff"
+  },
+  {
+    "revision": "64e4dd982ec30a04e66b5cd2b4159ee6",
+    "url": "/mypage/static/media/profile.64e4dd98.jpg"
+  }
+]);
